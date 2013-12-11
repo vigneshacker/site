7c4d0ee07a001d1a3cd39be8e7118591a4ca8db8
Dean Perry <dean@voupe.com>
2013-12-11 22:06:37 +0000

    logo & titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,19 @@
 class PagesController < ApplicationController
+  
   def home
+  	@title = "An Open Source Ruby on Rails Blogging Engine"
   end
 
   def about
+  	@title = "About"
   end
 
   def features
+  	@title = "Features"
   end
 
   def changelog
+  	@title = "Changelog"
   end
+
 end
